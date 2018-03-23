33c0205a5fb6ebdcf1550c6211d68a20b5979b57
sonalyc <36246561+sonalyc@users.noreply.github.com>
2018-03-23 18:41:11 -0300

    Update test_funcoes.py

--- test_funcoes.py
+++ test_funcoes.py
@@ -5,8 +5,8 @@ def test_answer():
     assert inc(3) == 5
     
     
-def teste1 (9_sai):
-  if (9_sai == 9):
+def teste1(X_sai, Y_entra):
+  if ((X_sai == 9) or (Y_entra == 9)):
     return True
   else:
     return False
